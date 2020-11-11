8212245e297884bd638a1934f0441e928353d1a2
Tetyana <tetyanajkn@gmail.com>
2020-11-11 21:41:54 +0200

    sorting string

--- les6.js
+++ les6.js
@@ -44,21 +44,36 @@ let numbers = [1, 5, 7, 11, 3, 8, 22, 10, 15, 199, 37000, 17, 256, 1024, 65536, 
 
 
 // - создать массив со словами на 15-20 элементов.
-let arrStr = ['Hello', 'world', 'lucky', 'house', 'Galaxy', 'Sun', 'function', 'stars', 'planet','Array', 'constructor', 'value', 'number', 'String', 'ok', 'green', 'text', 'happy']
+let arrStr = ['Hello', 'world', 'lucky', 'house', 'Galaxy', 'Sun', 'function', 'stars', 'planet', 'Array', 'constructor', 'value', 'number', 'String', 'ok', 'green', 'function', 'happy']
 // console.log(arrStr);
 
 
 // 7  -- отсортировать его по алфавиту в восходящем порядке.
-// let sortAZ = arrStr.sort();
+// let sortAZ = arrStr.sort((a, b) => {
+//     let aLow = a.toLowerCase();
+//     let bLow = b.toLowerCase();
+//     if (aLow > bLow){
+//         return 1;
+//     }
+//     if (aLow < bLow) {
+//         return -1;
+//     }
+//     return 0;
+// });
 // console.log(sortAZ);
 
 
 // 8 -- отсортировать его по алфавиту  в нисходящем порядке.
 // let sortZA = arrStr.sort((a, b) => {
-//     if (a > b){
-//         return -1
-//     } 
-//     return 1
+//     let aLow = a.toLowerCase();
+//     let bLow = b.toLowerCase();
+//     if (aLow> bLow) {
+//         return -1;
+//     }
+//     if (aLow < bLow) {
+//         return 1;
+//     }
+//     return 0;
 // });
 // console.log(sortZA);
 
@@ -81,17 +96,17 @@ let arrStr = ['Hello', 'world', 'lucky', 'house', 'Galaxy', 'Sun', 'function', '
 
 // Все робити через функції масивів (foreach, map ...тд)
 // Дан масив :
- let users = [ {name: 'vasya', age: 31, status: false}, 
-               {name: 'petya', age: 30, status: true}, 
-               {name: 'kolya', age: 29, status: true}, 
-               {name: 'olya', age: 28, status: false}, 
-               {name: 'max', age: 30, status: true}, 
-               {name: 'anya', age: 31, status: false}, 
-               {name: 'oleg', age: 28, status: false}, 
-               {name: 'andrey', age: 29, status: true}, 
-               {name: 'masha', age: 30, status: true}, 
-               {name: 'olya', age: 31, status: false}, 
-               {name: 'max', age: 31, status: true} ];
+let users = [{ name: 'vasya', age: 31, status: false },
+{ name: 'petya', age: 30, status: true },
+{ name: 'kolya', age: 29, status: true },
+{ name: 'olya', age: 28, status: false },
+{ name: 'max', age: 30, status: true },
+{ name: 'anya', age: 31, status: false },
+{ name: 'oleg', age: 28, status: false },
+{ name: 'andrey', age: 29, status: true },
+{ name: 'masha', age: 30, status: true },
+{ name: 'olya', age: 31, status: false },
+{ name: 'max', age: 31, status: true }];
 // console.log(users);
 
 
@@ -129,7 +144,7 @@ let arrStr = ['Hello', 'world', 'lucky', 'house', 'Galaxy', 'Sun', 'function', '
 // newUsers = newUsers.map((value, index) => {
 //    value.id = index
 //    return value
-    
+
 // });
 // console.log(newUsers);
 
@@ -144,19 +159,19 @@ let arrStr = ['Hello', 'world', 'lucky', 'house', 'Galaxy', 'Sun', 'function', '
 
 // 2  =============КЛАССНАЯ РАБОТА=================
 
-let cars = [ {producer:"subaru",model: "wrx",year: 2010, color:"blue",type: "sedan",engine: "ej204x",volume: 2,power: 400}, 
-            {producer:"subaru",model: "legacy",year: 2007, color:"silver",type: "sedan",engine: "ez30",volume: 3,power: 250}, 
-            {producer:"subaru",model: "tribeca",year: 2011, color:"white",type: "jeep",engine: "ej20",volume: 2,power: 300}, 
-            {producer:"subaru",model: "leone",year: 1998, color:"yellow",type: "sedan",engine: "ez20x",volume: 2,power: 140}, 
-            {producer:"subaru",model: "impreza",year: 2014, color:"red",type: "sedan",engine: "ej204x",volume: 2,power: 200}, 
-            {producer:"subaru",model: "outback",year: 2014, color:"red",type: "hachback",engine: "ej204",volume: 2,power: 165}, 
-            {producer:"bmw",model: "115",year: 2013, color:"red",type: "hachback",engine: "f15",volume: 1.5,power: 120}, 
-            {producer:"bmw",model: "315",year: 2010, color:"white",type: "sedan",engine: "f15",volume: 1.5, power: 120}, 
-            {producer:"bmw",model: "650",year: 2009, color:"black",type: "coupe",engine: "f60",volume: 6,power: 350}, 
-            {producer:"bmw",model: "320",year: 2012, color:"red",type: "sedan",engine: "f20",volume: 2,power: 180}, 
-            {producer:"mercedes",model: "e200",year: 1990, color:"silver",type: "sedan",engine: "eng200",volume: 2,power: 180}, 
-            {producer:"mercedes",model: "e63",year: 2017, color:"yellow",type: "sedan",engine: "amg63",volume:3,power: 400}, 
-            {producer:"mercedes",model: "c250",year: 2017, color:"red",type: "sedan",engine: "eng25",volume: 2.5,power: 230} ];
+let cars = [{ producer: "subaru", model: "wrx", year: 2010, color: "blue", type: "sedan", engine: "ej204x", volume: 2, power: 400 },
+{ producer: "subaru", model: "legacy", year: 2007, color: "silver", type: "sedan", engine: "ez30", volume: 3, power: 250 },
+{ producer: "subaru", model: "tribeca", year: 2011, color: "white", type: "jeep", engine: "ej20", volume: 2, power: 300 },
+{ producer: "subaru", model: "leone", year: 1998, color: "yellow", type: "sedan", engine: "ez20x", volume: 2, power: 140 },
+{ producer: "subaru", model: "impreza", year: 2014, color: "red", type: "sedan", engine: "ej204x", volume: 2, power: 200 },
+{ producer: "subaru", model: "outback", year: 2014, color: "red", type: "hachback", engine: "ej204", volume: 2, power: 165 },
+{ producer: "bmw", model: "115", year: 2013, color: "red", type: "hachback", engine: "f15", volume: 1.5, power: 120 },
+{ producer: "bmw", model: "315", year: 2010, color: "white", type: "sedan", engine: "f15", volume: 1.5, power: 120 },
+{ producer: "bmw", model: "650", year: 2009, color: "black", type: "coupe", engine: "f60", volume: 6, power: 350 },
+{ producer: "bmw", model: "320", year: 2012, color: "red", type: "sedan", engine: "f20", volume: 2, power: 180 },
+{ producer: "mercedes", model: "e200", year: 1990, color: "silver", type: "sedan", engine: "eng200", volume: 2, power: 180 },
+{ producer: "mercedes", model: "e63", year: 2017, color: "yellow", type: "sedan", engine: "amg63", volume: 3, power: 400 },
+{ producer: "mercedes", model: "c250", year: 2017, color: "red", type: "sedan", engine: "eng25", volume: 2.5, power: 230 }];
 // // Відфільтрувати масив за наступними крітеріями :
 // // 2.1 - двигун більше 3х літрів
 // let filterVolume3 = cars.filter((value) => {
@@ -242,17 +257,17 @@ let cars = [ {producer:"subaru",model: "wrx",year: 2010, color:"blue",type: "sed
 
 
 // - взять слдующий массив
-let usersWithAddress = [{id: 1, name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}}, 
-                        {id: 2, name: 'petya', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 1}}, 
-                        {id: 3, name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}}, 
-                        {id: 4, name: 'olya', age: 28, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 90}}, 
-                        {id: 5, name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}}, 
-                        {id: 6, name: 'anya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 2}}, 
-                        {id: 7, name: 'oleg', age: 28, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 22}}, 
-                        {id: 8, name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}}, 
-                        {id: 9, name: 'masha', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 12}}, 
-                        {id: 10, name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}}, 
-                        {id: 11, name: 'max', age: 31, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}}];
+let usersWithAddress = [{ id: 1, name: 'vasya', age: 31, status: false, address: { city: 'Lviv', street: 'Shevchenko', number: 16 } },
+{ id: 2, name: 'petya', age: 30, status: true, address: { city: 'Lviv', street: 'Shevchenko', number: 1 } },
+{ id: 3, name: 'kolya', age: 29, status: true, address: { city: 'Lviv', street: 'Shevchenko', number: 121 } },
+{ id: 4, name: 'olya', age: 28, status: false, address: { city: 'Lviv', street: 'Shevchenko', number: 90 } },
+{ id: 5, name: 'max', age: 30, status: true, address: { city: 'Lviv', street: 'Shevchenko', number: 115 } },
+{ id: 6, name: 'anya', age: 31, status: false, address: { city: 'Lviv', street: 'Shevchenko', number: 2 } },
+{ id: 7, name: 'oleg', age: 28, status: false, address: { city: 'Lviv', street: 'Shevchenko', number: 22 } },
+{ id: 8, name: 'andrey', age: 29, status: true, address: { city: 'Lviv', street: 'Shevchenko', number: 43 } },
+{ id: 9, name: 'masha', age: 30, status: true, address: { city: 'Lviv', street: 'Shevchenko', number: 12 } },
+{ id: 10, name: 'olya', age: 31, status: false, address: { city: 'Lviv', street: 'Shevchenko', number: 16 } },
+{ id: 11, name: 'max', age: 31, status: true, address: { city: 'Lviv', street: 'Shevchenko', number: 121 } }];
 // 2.13 -- отсортировать его по id пользователей
 // usersWithAddress.sort((a, b) => a.id-b.id);
 // console.log(usersWithAddress);
@@ -328,4 +343,3 @@ let usersWithAddress = [{id: 1, name: 'vasya', age: 31, status: false, address: 
 // let even = usersWithAddress.filter((value) => (!(value.address.number % 2)));
 // console.log(even);
 
- 
